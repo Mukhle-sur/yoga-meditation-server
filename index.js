3ecd7b39009651a026d69a93d1181925325ff2bc
Mukhles <mukhlesurrhaman43@gmail.com>
2023-06-11 03:07:51 +0600

    add classes and verify admin and instructor and get classes added

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ez2u9zk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,10 +49,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const instructorsCollection = client
-      .db("allYogaDB")
-      .collection("instructors");
+    const classesCollection = client.db("allYogaDB").collection("classes");
     const usersCollection = client.db("allYogaDB").collection("users");
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "Admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "Instructor") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -40,6 +83,17 @@ async function run() {
       res.send({ token });
     });
 
+    // class related api
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/addClasses", verifyJWT, verifyInstructor, async (req, res) => {
+      const instructorClass = req.body;
+      console.log(instructorClass);
+      const result = await classesCollection.insertOne(instructorClass);
+      res.send(result);
+    });
     // instructor related api
     app.get("/instructors", async (req, res) => {
       const query = { role: "Instructor" };
@@ -63,6 +117,7 @@ async function run() {
       res.send(result);
     });
 
+    // role change to user
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
@@ -85,6 +140,30 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // admin role define
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ Admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "Admin" };
+      res.send(result);
+    });
+
+    // instructor role define
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ Instructor: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "Instructor" };
       res.send(result);
     });
 
